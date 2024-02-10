a3dd74297ea28b0a9fffff83d1b8e2becd4e423d
marinus-g <marinusmail@protonmail.com>
2024-02-10 18:15:52 +0100

    temp updated backend

--- src/util/consts.ts
+++ src/util/consts.ts
@@ -1,4 +1,4 @@
-export const BACKEND = 'http://localhost:8080';
+export const BACKEND = 'ADDRESS';
 export enum END_POINT {
   DNS_RESOLVE = BACKEND + '/api/v1/public/dns/',
   WEATHER = BACKEND + '/api/v1/public/weather',
